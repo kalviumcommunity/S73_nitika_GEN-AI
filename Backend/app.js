10b21d7371eb2ae2df7a07eb980fe1224b5dab42
Nitika-singh-4 <Nitikasingh262@gmail.com>
2025-08-28 15:56:37 +0530

    One shot promt completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 // import { runSystemUser } from "./prompts/systemUser.js";
-import { runZeroShot } from "./prompts/zeroShot.js";
+// import { runZeroShot } from "./prompts/zeroShot.js";
+import { runOneShot } from "./prompts/oneShot.js";
 // later import others...
 
 dotenv.config();
@@ -11,4 +12,5 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 // choose which one to run
 // await runSystemUser(genAI);
 
-await runZeroShot(genAI);
+// await runZeroShot(genAI);
+await runOneShot(genAI);
